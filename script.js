ac1923acaf200ff89d8947efa9af6a461038f4af
tirkia21 <84148483+tirkia21@users.noreply.github.com>
2021-06-05 00:20:24 +0400

    add.script.js

---
+++ script.js
@@ -0,0 +1,77 @@
+// amocana 1
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+// // for (let item of numbers) {
+// //     if (item % 2 == 0) {
+// //         console.log(item);
+// //     }
+// // }
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if(numbers[i] % 2 == 1) {
+//         console.log(numbers[i]);
+//     }
+// }
+
+
+// amocna 2
+// var person = {
+//     firstname: "john",
+//     lastname: "doe",
+//     age: 50,
+//     eyecolor: "blue"
+
+// }
+
+// console.log(person.eyecolor);
+
+// amocana 3
+// let cars = ["audi","bmw", "lexus", "volkswagen", " por", "audi", "sche"]; 
+
+// console.log(cars[2],cars[3],cars[4],cars[5]);
+
+
+// amocana 4
+// let car = ['audi', 'porsche', 'lexus', 'bmw', ['volkswagn', 'Chevrolet', 'Ferrari'] ];
+
+// console.log(car[4][2]);
+
+// amocana 5}
+
+
+
+// amocana 6
+// let x = 6;
+// let y = '6';
+
+// console.log(x === y);
+
+// სამი ტოლობის შეთხვევში არ ხდება ტიპების კონვერტაცია ანი x არის Number ხოლო y არის String ამიტომ ასეთ დროს დააბრუნებს false
+
+
+
+// amocana 7
+// let numbers = [1, 2, 3, 4, 5, 6];
+
+// // console.log(numbers[0]);
+// // console.log(numbers[1]);
+// // console.log(numbers[2]);
+// // console.log(numbers[3]);
+// // console.log(numbers[4]);
+// // console.log(numbers[5]);
+
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+
+
+
+
+// amocana 8
+// let names = ['ia','nika','giorgi','gia','pavle']
+
+// for (let i = 0; i < names.length; i++){
+//     if (names[i].length > 3) {
+//         console.log(names[i]);
+//     }
+// }
